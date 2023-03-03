1a9549ab36476da0e7e0251b46b376b77c125569
kovalski.dev@gmail.com <kovalski.dev@gmail.com>
2023-03-03 17:58:11 +0300

    Created navbar component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,17 +3,16 @@ import "./styles/index.scss";
 import { AppRouter } from "./providers/router";
 import { useTheme } from "./providers/ThemeProvider";
 import { classNames } from "../shared/lib/classNames/classNames";
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
-      <button onClick={toggleTheme}>Change theme</button>
-      <Link to={"/"}>General</Link>
-      <Link to={"/about"}>About</Link>
-
+      <Navbar />
       <AppRouter />
+      <button onClick={toggleTheme}>Change theme</button>
     </div>
   );
 };
