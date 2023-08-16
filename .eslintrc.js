6b34cb1a4e738a5180c559c991abe9bd61f6a075
kovalski.dev@gmail.com <kovalski.dev@gmail.com>
2023-08-16 18:27:42 +0300

    Added redux-toolkit

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,7 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'error',
     'no-underscore-dangle': 'off',
+    'no-param-reassign': 'off',
     'linebreak-style': 0,
     'i18next/no-literal-string': [
       'error',
