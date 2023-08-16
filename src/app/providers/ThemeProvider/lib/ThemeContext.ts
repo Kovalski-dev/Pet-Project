21d374b6a35ea6bd9cec901545a4c94f7d52b12d
Nikita Koval <69389602+Kovalski-dev@users.noreply.github.com>
2023-08-16 22:47:18 +0300

    Added modal themes, fix styles (#31)

--- src/app/providers/ThemeProvider/lib/ThemeContext.ts
+++ src/app/providers/ThemeProvider/lib/ThemeContext.ts
@@ -1,8 +1,8 @@
 import { createContext } from 'react';
 
 export enum Theme {
-  LIGNT = 'default',
-  DARK = 'dark',
+  LIGNT = 'app_default_theme',
+  DARK = 'app_dark_theme',
 }
 
 export interface ThemeContextProps {
--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -2,6 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import {
   ReactNode, MouseEvent, useState, useRef, useEffect, useCallback,
 } from 'react';
+import { useTheme } from 'app/providers/ThemeProvider';
 import cls from './Modal.module.scss';
 import Portal from '../Portal/Portal';
 
@@ -21,6 +22,7 @@ export const Modal = (props : ModalProps) => {
 
   const [isClosing, setIsClosing] = useState(false);
   const timerRef = useRef<ReturnType<typeof setTimeout>>();
+  const { theme } = useTheme();
 
   const closeHandler = useCallback(() => {
     if (onClose) {
@@ -57,7 +59,7 @@ export const Modal = (props : ModalProps) => {
 
   return (
     <Portal>
-      <div className={classNames(cls.Modal, mods, [className])}>
+      <div className={classNames(cls.Modal, mods, [className, theme])}>
         <div className={cls.overlay} onClick={closeHandler}>
           <div className={cls.content} onClick={onContentClick}>{children}</div>
         </div>
